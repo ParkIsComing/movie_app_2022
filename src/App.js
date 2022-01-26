4d35db3ced12244a903f2faa65f2de49edb8af08
ParkIsComing <maroony55@gmail.com>
2022-01-26 18:22:33 +0900

    클래스형 컴포넌트 생명주기 함수

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 class App extends React.Component{  /*클래스형 컴포넌트*/
+  constructor(props) {
+    super(props);
+    console.log('hello');
+  }
+
   state={
     count:0,
 
@@ -14,12 +19,22 @@ class App extends React.Component{  /*클래스형 컴포넌트*/
     this.setState(current => ({count: current.count-1}));
   }
 
-  render(){ /* 함수형 컴포넌트는 return문으로  클래스형 컴포넌트 render()로 JSX 반환*/
-    return (<div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>); /*현재 클래스에 선언한 state를 지칭하기 위해 this.state*/
+  componentDidMount(){ /*render()가 실행된 이후 componentDidMount()가 실행됨*/
+    console.log('component rendered')
   }
+
+  componentDidUpdate(){
+    console.log('I just updated')
+  }
+
+  render(){
+    console.log("I'm rendering");
+    return(
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )  }
 }
 export default App;
